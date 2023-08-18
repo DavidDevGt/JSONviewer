b9232606ba32393efe63c94e05e087bcd6ca6c98
Davidvl <josuedavidvl18@gmail.com>
2023-08-18 02:02:29 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,67 @@
+new Vue({
+    el: '#jsonEditorApp',
+    data: {
+        jsonInput: '',
+        error: null,
+        formattedJSON: '',
+        mode: 'light',
+    },
+    computed: {
+        modeButtonText() {
+            return this.mode === 'light' ? 'Cambiar a modo oscuro' : 'Cambiar a modo claro';
+        }
+    },
+    watch: {
+        mode(newVal) {
+            document.body.setAttribute('class', newVal);
+        }
+    },
+    methods: {
+        toggleMode() {
+            this.mode = this.mode === 'light' ? 'dark' : 'light';
+        },
+        validateJSON() {
+            try {
+                JSON.parse(this.jsonInput);
+                this.error = null;
+            } catch (err) {
+                this.error = 'JSON inválido';
+            }
+        },
+        formatJSON() {
+            try {
+                const jsonObject = JSON.parse(this.jsonInput);
+                this.formattedJSON = JSON.stringify(jsonObject, null, 4);
+                this.error = null;
+            } catch (err) {
+                this.error = 'JSON inválido';
+            }
+        },
+        minimizeJSON() {
+            try {
+                const jsonObject = JSON.parse(this.jsonInput);
+                this.formattedJSON = JSON.stringify(jsonObject);
+                this.error = null;
+            } catch (err) {
+                this.error = 'JSON inválido';
+            }
+        },
+        clear() {
+            this.jsonInput = '';
+            this.formattedJSON = '';
+        },
+        // Método para copiar el JSON formateado
+        copyOutput() {
+            const el = document.createElement('textarea');
+            el.value = this.formattedJSON;
+            document.body.appendChild(el);
+            el.select();
+            document.execCommand('copy');
+            document.body.removeChild(el);
+            alert("Output copiado al portapapeles");
+        }
+    },
+    mounted() {
+        this.toggleMode();
+    }
+});
